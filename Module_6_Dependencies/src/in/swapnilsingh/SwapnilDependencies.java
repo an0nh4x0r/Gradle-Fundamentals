98f166fffdac486067d97886129ca22a3cadfd39
swapnil singh <swappp7singh@gmail.com>
2018-09-01 21:14:05 +0530

    jcenter repository and dependencies from mvnrepository.com

---
+++ Module_6_Dependencies/src/in/swapnilsingh/SwapnilDependencies.java
@@ -0,0 +1,9 @@
+package in.swapnilsingh;
+
+import org.apache.logging.log4j.*;
+
+public class SwapnilDependencies {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ Module_6_Dependencies/test/in/swapnilsingh/TestSwapnilDependencies.java
@@ -0,0 +1,7 @@
+package in.swapnilsingh;
+
+import org.junit.*;
+
+public class TestSwapnilDependencies {
+
+}
