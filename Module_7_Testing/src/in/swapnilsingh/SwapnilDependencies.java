8f3e639612d751eac614b12cf2f45a2a3d4d4ba9
swapnil singh <swappp7singh@gmail.com>
2018-09-08 13:21:31 +0530

    Unable to complete this module. Have very little knowledge of jUnit.

---
+++ Module_7_Testing/src/in/swapnilsingh/SwapnilDependencies.java
@@ -0,0 +1,15 @@
+package in.swapnilsingh;
+
+public class SwapnilDependencies {
+
+    public static void main(String[] args) {
+        SwapnilDependencies swapnilDependencies =
+                new SwapnilDependencies();
+        System.out.println(swapnilDependencies.addNums(1, 2));
+    }
+
+    public int addNums(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ Module_7_Testing/test/in/swapnilsingh/SwapnilDependenciesTest.java
@@ -0,0 +1,15 @@
+package in.swapnilsingh;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SwapnilDependenciesTest {
+
+    @Test
+    public void testAddNums() {
+        assertEquals(2, 2);
+    }
+
+}
